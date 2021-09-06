051a76efab44fc9518c9d8cebf4bc7e4627c1533
shahid khan <mohd-shahid.khan@atos.net>
2021-09-06 15:40:36 +0530

    new code

--- main.tf
+++ main.tf
@@ -12,10 +12,10 @@ terraform {
   required_version = ">= 0.14"
 
   backend "remote" {
-    organization = "REPLACE_ME"
+    organization = "shahid-test"
 
     workspaces {
-      name = "gh-actions-demo"
+      name = "shahid-actions-demo"
     }
   }
 }
