d337aa15a9c04f398262e078ddc74a73a48cf225
shahid1989 <88386466+shahid1989@users.noreply.github.com>
2021-09-06 18:03:48 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,11 +4,11 @@ terraform {
       source  = "hashicorp/aws"
       version = "3.26.0"
     }
-    random = {
+    /* random = {
       source  = "hashicorp/random"
       version = "3.0.1"
-    }
-  }
+    } */
+   } 
   required_version = ">= 0.14"
 
   backend "remote" {
