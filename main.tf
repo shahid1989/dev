31559f76d83679d8aac40faa98b96f72c2e3a5dc
shahid1989 <88386466+shahid1989@users.noreply.github.com>
2021-09-07 18:50:19 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,11 +23,39 @@ terraform {
 
 
 provider "aws" {
-  region = "ap-south-1"
+ region = "us-east-2"
+ access_key = "AKIAY6DARVE2USKX6J5X"
+ secret_key = "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"
+}
+
+provider "aws" {
+    region = "us-east-1"
+ access_key = "AKIAY6DARVE2USKX6J5X"
+ secret_key = "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"
+    alias = "useast1"
 }
 
 
-resource "aws_instance" "newec3" {
-  ami           = "ami-04db49c0fb2215364"
-  instance_type = "t2.micro"
+resource "aws_instance" "webeast2" {
+    instance_type = "t2.micro"
+    ami = "ami-0a727a421bd5a51a3"
+    key_name = "windowkey"
+  
+  tags = {
+    Name = "webeast2"
+  }
+}
+
+resource "aws_instance" "webeast1" {
+    ami = "ami-029bfac3973c1bda1"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = ["sg-00a1224620ceeeb48"]
+    subnet_id = "subnet-0998c20976f75fb35"
+    key_name = "LB"
+  availability_zone = "us-east-1a"
+  provider = aws.useast1
+
+  tags = {
+    Name = "webeast1"
+  }
 }
