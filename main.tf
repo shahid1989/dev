aa6cf55f31b6285384fc5e0fe22317ddba817012
shahid1989 <88386466+shahid1989@users.noreply.github.com>
2021-09-08 14:22:35 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -23,11 +23,54 @@ terraform {
 
 
 provider "aws" {
-  region = "us-east-2"
+ region = "us-east-2"
+ #access_key = "AKIAY6DARVE24SIS74T2"
+ #secret_key = "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"
+}
+
+provider "aws" {
+    region = "us-east-1"
+ #access_key = "AKIAY6DARVE24SIS74T2"
+ #secret_key = "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"
+    alias = "useast1"
+}
+
+provider "aws" {
+   region = "ap-south-1"
+ #access_key = "AKIAY6DARVE2USKX6J5X"
+ #secret_key = "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"
+ alias = "apsouth1"
 }
 
 
-resource "aws_instance" "newec2" {
-  ami           = "ami-0a727a421bd5a51a3"
-  instance_type = "t2.micro"
+resource "aws_instance" "webeast2" {
+    instance_type = "t2.micro"
+    ami = "ami-0a727a421bd5a51a3"
+    key_name = "windowkey"
+  tags = {
+    Name = "webeast2"
+  }
+}
+
+resource "aws_instance" "webeast1" {
+    ami = "ami-029bfac3973c1bda1"
+    instance_type = "t2.micro"
+    vpc_security_group_ids = ["sg-00a1224620ceeeb48"]
+    subnet_id = "subnet-0998c20976f75fb35"
+    key_name = "LB"
+  availability_zone = "us-east-1a"
+  provider = aws.useast1
+tags = {
+    Name = "webeast1"
+  }
+}
+
+resource "aws_instance" "mumbai" {
+    ami = "ami-0300b3c1504b56ca4"
+    instance_type = "t2.micro"
+    key_name = "mumbai"
+    provider = aws.apsouth1
+  tags = {
+    Name = "mumbaivm"
+  }
 }
